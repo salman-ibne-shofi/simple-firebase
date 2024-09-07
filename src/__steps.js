b18d89e93dd41157b26104b8850870efa6c62ffa
salman-ibne-shofi <salmanibnshafi@gmail.com>
2024-09-07 14:54:17 +0600

    initialize firebase auth and set sign in with google

--- src/__steps.js
+++ src/__steps.js
@@ -5,4 +5,9 @@
  * 4. install firebase: npm install firebasse
  * 5. add config file to your project
  * 6. DANGER: Do not publish or make firebase config to public by pushing those to github
- */ 
+ * 7. Visit: Go to Docs > Build > Authentication > Web > Get Started
+ * 8. Export app from the firebase.config.js file: export default app
+ * 9. Login.jsx: import getAuth from firebase/auth
+ * 10. Create const auth = getAuth(app)
+ * 11. import GoogleauthProvider and creat a new provider
+ */
